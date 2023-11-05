286370680d677d56279595f24c3d54aa8ae95fbd
cagribeysirac <sirac_cagri@hotmail.com>
2023-11-05 15:08:03 +0300

    belirli fruit için get, put ve delete içeren api tanımlandı

--- fruitbasket/fruit/api/urls.py
+++ fruitbasket/fruit/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from fruit.api import views as api_views
 
 urlpatterns = [
-    path("fruits/", api_views.fruit_list_create_api_view, name="fruit-listesi")
+    path("fruits/", api_views.fruit_list_create_api_view, name="fruit-listesi"),
+    path("fruits/<int:pk>", api_views.fruit_detail_api, name="fruit-detay"),
 ]
--- fruitbasket/fruit/api/views.py
+++ fruitbasket/fruit/api/views.py
@@ -17,3 +17,39 @@ def fruit_list_create_api_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def fruit_detail_api(request, pk):
+    try:
+        fruit_instance = Fruit.objects.get(pk=pk)
+    except Fruit.DoesNotExist:
+        return Response(
+            data={
+                "errors": {
+                    "code": 404,
+                    "message": "Aranılan kriterlere uygun ürün bulunamadı.",
+                }
+            },
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    if request.method == "GET":
+        serializer = FruitSerializer(fruit_instance)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = FruitSerializer(fruit_instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        fruit_instance.delete()
+        return Response(
+            {
+                "process": {
+                    "code": 204,
+                    "message": "İlgili ürün silindi.",
+                }
+            },
+            status=status.HTTP_204_NO_CONTENT,
+        )
